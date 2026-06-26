8269e4d151beaa6787534130b6cb33a13061c743
James Trew <j.trew10@gmail.com>
2026-06-25 20:49:00 -0400

    feat: add agents return-to-dashboard command (#28)

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import registerRollback from "./rollback.js";
 import registerPromptNormalization from "./prompt-normalization/index.js";
 import registerSpotme from "./spotme/index.js";
 import registerInlineReferences from "./inline-references/index.js";
-import registerTauStatus from "./tau-status/index.js";
+import registerTau from "./tau/index.js";
 import { type ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { loadConfig, registerBitesCommands, type SnacksConfig } from "./config.js";
 
@@ -40,7 +40,7 @@ export default function (pi: ExtensionAPI) {
 
   if (isExploreSubagent) return;
 
-  if (!isNonInteractive && !disabled.has("tauStatus")) registerTauStatus(pi);
+  if (!isNonInteractive && !disabled.has("tau")) registerTau(pi);
 
   if (!isNonInteractive && !disabled.has("footer")) registerFooter(pi);
   if (!isNonInteractive && !disabled.has("statusline")) registerStatusline(pi, configRef);
